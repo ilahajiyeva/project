d502bf6deff94b012642aa4e1a45f8aba52e506d
ilahajiyeva <44476352+ilahajiyeva@users.noreply.github.com>
2024-11-06 16:06:19 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+
+require "./db.php";
+
+//cover
+$stmt = $conn->prepare("SELECT * FROM pages WHERE id = 1");
+$stmt->execute();
+$page = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$title = $page['title'];
+$description = $page['description'];
+$image = $page['image'];
+
+require "./frontend/layouts/navbar.php";
+require "./frontend/layouts/header.php";
+
+//pagination
+$limit = 3;
+if(!isset($_GET['page'])) {
+    $page = 2;
+    $offset = 0;
+}else {
+    $page = $_GET['page'] + 1;
+    $offset =  ($_GET['page'] -1) * $limit;
+}
+
+$sql = "SELECT posts.*, categories.id as category_id, categories.name as category_name FROM posts 
+LEFT JOIN categories ON categories.id=posts.category_id ORDER BY posts.id DESC LIMIT $limit OFFSET $offset";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$pageUrl = $_SERVER['REQUEST_SCHEME']. "://" . $_SERVER['HTTP_HOST'] . $_SERVER["SCRIPT_NAME"];
+
+
+?>
+        
+<!-- Main Content-->
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-md-10 col-lg-8 col-xl-7">
+                    <!-- Post preview-->
+                     <?php foreach($posts as $post) {?>
+                    <div class="post-preview">
+                        <a href="./frontend/sample-post.php?id=<?= $post['id']?>">
+                            <h2 class="post-title"><?= $post['title']?></h2>
+                            <?php if (!empty($post['image'])): ?>
+										<img src="<?= '/project' . $post['image'] ?>" 
+										 width="80%" />
+							<?php endif; ?>
+                            <h3 class="post-subtitle"><?= $post['description']?></h3>
+                        </a>
+                        <p class="post-meta">
+                            Category
+                        <a href="<?= $baseUrl . "/project/frontend/categories.php?category_id=" . $post['category_id']?>">
+                            <?= $post['category_name']?>
+                        </a>
+                        </p>
+                    </div>
+                    <?php }?>
+                    <!-- Pager-->
+                    <div class="d-flex justify-content-end mb-4 justify-content-between">
+                    <?php
+                     if(isset($_GET['page']) && $_GET['page']>1) {                    
+                     ?>
+                    <a class="btn btn-primary text-uppercase" href="<?=$pageUrl . "?page=".$page-2?>">Previous</a>
+                    <?php }?>
+                    <a class="btn btn-primary text-uppercase ml-2" href="<?=$pageUrl . "?page=".$page?>">Next</a>
+                </div>
+            </div>
+        </div>
+
+        <?php
+
+require "./frontend/layouts/footer.php";
+
+?>
+        
